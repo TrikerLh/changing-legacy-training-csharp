35e9c389df8f04304e951e3cb001564934a8ae8d
trikitrok <trikitrok@gmail.com>
2024-04-23 00:32:09 +0200

    adding starting point with broken dependencies for an easier version of Crazy Portfolio

---
+++ more-simplified-crazy-portfolio-after-breaking-dependencies/Portfolio.Tests/helpers/TestingPortfolioBuilder.cs
@@ -0,0 +1,35 @@
+namespace Portfolio.Tests.helpers;
+
+public class TestingPortfolioBuilder
+{
+    private readonly List<string> _lines;
+    private string _now;
+
+    private TestingPortfolioBuilder()
+    {
+        _lines = new List<string>();
+        _now = "";
+    }
+
+    public static TestingPortfolioBuilder APortFolio()
+    {
+        return new TestingPortfolioBuilder();
+    }
+
+    public TestingPortfolioBuilder With(AssetsFileLinesBuilder lineBuilder)
+    {
+        _lines.Add(lineBuilder.Build());
+        return this;
+    }
+
+    public TestingPortfolioBuilder OnDate(string dateAsString)
+    {
+        _now = dateAsString;
+        return this;
+    }
+
+    public TestingPortfolio Build()
+    {
+        return new TestingPortfolio(_lines.ToArray(), _now);
+    }
+}
